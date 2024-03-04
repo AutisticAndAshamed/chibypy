4bcd234331ddea26f20f1e2c748115db7127c825
AutisticAndAshamed <98008253+AutisticAndAshamed@users.noreply.github.com>
2024-03-04 00:18:46 -0700

    chibypy file defuck

--- chibypy.py
+++ chibypy.py
@@ -1,8 +1,18 @@
-/* This program is free software. It comes without any warranty, to
+
+
+
+
+""" 
+
+    https://www.github.com/AutisticAndAshamed/ChiByPy
+
+    This program is free software. It comes without any warranty, to
      * the extent permitted by applicable law. You can redistribute it
      * and/or modify it under the terms of the Do What The Fuck You Want
      * To Public License, Version 2, as published by Sam Hocevar. See
-     * http://www.wtfpl.net/ for more details. */
+     * http://www.wtfpl.net/ for more details.
+
+"""
 
 
 def chiplot(xdata, ydata, fitfunction, vars, freevarrange, freevarint, sigma): 
