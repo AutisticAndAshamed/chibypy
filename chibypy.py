b7d5b2f5a94e06d1da70e85e4356ab7498c4f9b2
AutisticAndAshamed <98008253+AutisticAndAshamed@users.noreply.github.com>
2024-03-12 04:45:25 -0700

    chiplot sorting algorithm update
    Made uncertainty sorting algorithm less retarded and inefficient using numpy.argsort function

--- chibypy.py
+++ chibypy.py
@@ -3,7 +3,6 @@
 
 
 """ 
-
     https://www.github.com/AutisticAndAshamed/ChiByPy
 
     This program is free software. It comes without any warranty, to
@@ -15,32 +14,25 @@
 """
 
 
-def chiplot(xdata, ydata, fitfunction, vars, freevarrange, freevarint, sigma): 
+def chiplot(xdata, ydata, sigma, fitfunction, vars, freevarrange, freevarint): 
     
     vars = list(vars)
-    fit = np.zeros(len(freevarrange))
-    chivalue = np.zeros(len(freevarrange))
+    fit = numpy.zeros(len(freevarrange))
+    chivalue = numpy.zeros(len(freevarrange))
     
 
-    for i, point in enumerate(freevarrange):
+    for i in enumerate(freevarrange):
         
         vars[freevarint] = freevarrange[i]
         fit = fitfunction(xdata, *vars)
         dev = ((ydata - fit)**2) / sigma ** 2
-        chivalue[i] = np.sum(dev)
+        chivalue[i] = numpy.sum(dev)
 
-    chimin = np.min(chivalue)
-    variablevalue = freevarrange[np.where(chivalue == float(chimin))]
+    chimin = numpy.min(chivalue)
+    variablevalue = freevarrange[numpy.where(chivalue == float(chimin))]
 
-    searchprecision = 0
-    while True:
-        uncertainty = freevarrange[np.where(abs(chimin + 1 - chivalue) < searchprecision)]
-        searchprecision += 1e-3 * np.mean(np.diff(freevarrange))
-        if len(uncertainty) == 2:
-            break
-        elif len(uncertainty) > 2:
-            print('Error: returned incorrect number of uncertainty values')
-            break
-    uncertainty = uncertainty - variablevalue
+    uncertainty_indices = numpy.argsort(numpy.abs(chivalue - (chimin + 1)))[:2]
+    uncertainty = numpy.sort(freevarrange[uncertainty_indices] - variablevalue)
 
-    return chivalue, chimin, variablevalue, uncertainty
+    return chivalue, variablevalue, uncertainty
+
